e79b935e12f702565830048f777a5049ea0447c4
Ryan Chen <xryanchen@gmail.com>
2018-04-20 15:59:39 -0700

    Share worker now sends heart beat information

--- worker.js
+++ worker.js
@@ -1,13 +1,23 @@
+let ports = [],
+    count = 0
+
 this.addEventListener('message', (msg) => {
   console.log('Message received:', msg.data)
 })
 
 this.addEventListener('connect', (msg) => {
   let port = msg.ports[0]
+  ports.push(port)
+
   port.start()
   port.addEventListener('message', (msg) => {
     console.log('received', msg)
   })
-
-  port.postMessage('from worker')
 })
+
+setInterval(() => {
+  count++
+  ports.forEach((p) => {
+    p.postMessage(`from worker: ${count}`)
+  })
+}, 500)
